1444c3cb80f13240d2b6a26a36787c3618778857
BiriukovAlex <alex7birukov77@gmail.com>
2022-10-07 21:01:22 +0300

    Create Умножение.py

---
+++ Умножение.py
@@ -0,0 +1,30 @@
+import random
+
+life=3
+time=1
+
+while 1:
+	a=random.randrange(0, 10, 1)
+	b=random.randrange(0, 10, 1)
+	
+	while 1:
+		try:
+			c=int(input("Жизнь #{} Пример#{} Решай:{}*{}=".format(life,time,a,b)))
+			break
+		except ValueError:
+			print ("ОШИБКА! Не верное число. Повторите ввод!")
+			continue
+			
+	d=a*b
+	if d==c:
+		print ("Правильно")
+	else:
+		print ("Не правильно! минус жизнь.")
+		life=life-1
+	
+	if life==0:
+		print ("Учи таблицу!")
+		break
+
+
+
